a7004e290988dea52cffc01b6b20971f14733b56
Ben Nguyen <benpeternguyen@gmail.com>
2025-01-25 13:32:39 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+function hamburg(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(0px)"
+}
+
+function cancel(){
+    const navbar = document.querySelector(".dropdown")
+    navbar.style.transform = "translateY(-500px)"
+}
+
+const texts = [
+    "DEVELOPER",
+    "STUDENT"
+]
+
+let speed = 100;
+
+const textElements = document.querySelector(".typewriter-text");
+
+let textIndex = 0;
+let characterIndex = 0;
+
+function typewriter(){
+    if(characterIndex < texts[textIndex].length){
+        textElements.innerHTML += texts[textIndex].charAt(characterIndex);
+        characterIndex++;
+        setTimeout(typewriter, speed);
+    } else {
+        setTimeout(eraseText, 1000);
+    }
+}
+
+window.onload = typewriter;
+
+function eraseText(){
+    if(textElements.innerHTML.length > 0){
+        textElements.innerHTML = textElements.innerHTML.slice(0, -1);
+        setTimeout(eraseText, 50)
+    } else {
+        textIndex = (textIndex + 1) % texts.length;
+        characterIndex = 0;
+        setTimeout(typewriter, 500);
+    }
+}
